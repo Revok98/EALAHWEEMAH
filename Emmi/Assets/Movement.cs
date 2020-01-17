0f0725a037bc7f7f954d323f612247e64d8bbd66
DUCHEMIN Emeric <educhemin001@enseirb-matmeca.fr>
2020-01-18 00:06:30 +0100

    Add several scripts

---
+++ Emmi/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Code produit en partie par Unity et remodelé par Alexandre Rousseau. Réutilisation avec son aimable accord. 
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Vector3 movement;
+    private Rigidbody playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Move(h, v);
+    }
+
+    void Move(float h, float v) {
+        movement.Set(h, 0f, v);
+        movement = Camera.main.transform.TransformDirection(movement).normalized;//Penser à changer en fonction de la position de la caméra 
+        movement.y = 0.0f;
+        Debug.Log(movement);
+        movement = movement * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
