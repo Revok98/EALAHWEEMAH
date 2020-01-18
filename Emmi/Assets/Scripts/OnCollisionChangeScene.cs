cb716ae5b3b241eac8c3a1a8711d3d4ba140dfed
clemetayer <c.lemetayer@laposte.net>
2020-01-18 10:23:54 +0100

    added collisions to change scene

---
+++ Emmi/Assets/Scripts/OnCollisionChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionChangeScene : MonoBehaviour
+{
+    public string SceneToGo;
+    public GameObject target;
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == target.tag)
+        {
+            Application.LoadLevel(SceneToGo);
+        }
+    }
+}
