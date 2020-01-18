5dcd5509efdd3c2fbfee315447eb75011fe7a749
DUCHEMIN Emeric <educhemin001@enseirb-matmeca.fr>
2020-01-18 09:19:06 +0100

    Add the scripts in the test scene

---
+++ Emmi/Assets/Scripts/DecreasingController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecreasingController : MonoBehaviour
+{
+    Vector3 CurrentVolume;
+    public float PourcentageToDecrease;
+    Vector3 Vect;
+    public float DeadUnder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentVolume = this.transform.localScale;
+        Vect = new Vector3(PourcentageToDecrease, PourcentageToDecrease, PourcentageToDecrease); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentVolume = CurrentVolume - Vect;
+        this.transform.localScale = CurrentVolume;
+    }
+
+    public bool isTooSmall() {
+        if (CurrentVolume.x<= DeadUnder) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Emmi/Assets/Scripts/MagnetismManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MagnetismManager : MonoBehaviour
+{
+    public Image Bar;
+    public float Fill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fill = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Bar.fillAmount = Fill;
+        Fill = Fill - 0.01f;
+    }
+}
