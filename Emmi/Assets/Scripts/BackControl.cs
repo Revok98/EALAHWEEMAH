052295806feaa10f8f4a5ef3c8eac28b291d09f6
DUCHEMIN Emeric <educhemin001@enseirb-matmeca.fr>
2020-01-19 01:01:58 +0100

    Add the script to control the ennemies

---
+++ Emmi/Assets/Scripts/BackControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackControl : MonoBehaviour
+{
+
+    public string menu_scene = "";
+    public void getBack() {
+        SceneManager.LoadScene(menu_scene);
+    }
+}
---
+++ Emmi/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public int circleRadius;
+    Vector3 truePosition;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        truePosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 diff = player.GetComponent<Rigidbody>().transform.position - truePosition;
+        if (Mathf.Pow(diff.x,2) + Mathf.Pow(diff.y,2) <= circleRadius) {
+            Move();
+        }
+    }
+    private void Move()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = Vector3.MoveTowards(transform.position, player.GetComponent<Rigidbody>().transform.position, speed);
+    }
+}
